59068d25063b5b1417a83edf7e6cdacaeaca90b5
skyblue135-pixel <lotte222@gmail.com>
2026-06-11 16:18:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -130,4 +130,5 @@ if uploaded_file is not None:
                     document_chain
                 )
 
-                qa_chain.invoke(    {    "input": question    }      )
+                response = qa_chain.invoke(    {    "input": question    }      )
+                st.write(  response["answer"]    )
